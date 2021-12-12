a10a20245280c1c58055422bac5a358df95559ac
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-12-12 20:38:15 +0100

    patch : copyUrl by alt+l

--- config.def.h
+++ config.def.h
@@ -168,6 +168,7 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+
  	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
  	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
@@ -197,6 +198,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+  // add by me
+	{ MODKEY,               XK_l,    copyurl,        {.i =  0} },
 };
 
 /*
